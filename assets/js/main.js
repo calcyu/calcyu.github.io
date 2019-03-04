f5c0aeffb088a110674acccd21e3738311c1ef47
calcyu <hi@geek5.cn>
2019-03-04 23:47:27 +0800

    vssue

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,24 +48,24 @@ $(function() {
     });
 
     // discus comment.
-      if($("#disqus_thread").length>0){
-        var ds_loaded = false;
-        window.disqus_shortname = "{{ site.disqus.shortname }}";
-        main.scroll(function(){
-          var nScrollHight = $(this)[0].scrollHeight;
-          var nScrollTop = $(this)[0].scrollTop;
-          if(!ds_loaded && nScrollTop + main.height() >= nScrollHight - 100) {
-            $.ajax({
-              type: 'GET',
-              url: 'http://' + disqus_shortname + '.disqus.com/embed.js',
-              dataType: 'script',
-              cache: true
-            });
-            ds_loaded = true;
-          }
-        });
+      if($("#vssue").length>0){
+        new Vue({
+          el: '#vssue',
+          render: h => h('Vssue', {
+            props: {
+              // 在这里设置当前页面对应的 Issue 标题
+              title: 'Vssue Dev',
+              // 在这里设置你使用的平台的 OAuth App 配置
+              options: {
+                owner: 'calcyu',
+                repo: 'temp',
+                clientId: '92562dc4e8e1afdc8a19',
+                clientSecret: 'ad9586cb956a0bac93e167fc004faf90fa26dd86',
+              },
+            }
+          })
+        })
       }
-    {% endif %}
     // your scripts
   };
   afterPjax();
