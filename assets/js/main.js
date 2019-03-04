241851b7795b1ecb611bd45c93d71fa543637b73
calcyu <calcyu@qq.com>
2019-03-05 03:22:32 +0800

    vssue评论功能

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,13 +54,15 @@ $(function() {
           render: h => h('Vssue', {
             props: {
               // 在这里设置当前页面对应的 Issue 标题
-              title: 'Vssue Dev',
+              title: $(".post-title").text(),
               // 在这里设置你使用的平台的 OAuth App 配置
               options: {
-                owner: 'calcyu',
-                repo: 'temp',
-                clientId: '92562dc4e8e1afdc8a19',
-                clientSecret: 'ad9586cb956a0bac93e167fc004faf90fa26dd86',
+                owner: '{{site.vssue.owner}}',
+                repo: '{{site.vssue.repo}}',
+                clientId: '{{site.vssue.client_id}}',
+                clientSecret: '{{site.vssue.client_secret}}',
+                labels: ['{{site.vssue.labels}}'],
+                prefix: '[{{site.vssue.prefix}}]'
               },
             }
           })
